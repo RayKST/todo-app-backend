2def9e42e4606b1fe262952d3500fbfe20b63981
Kauan <jailma.kmp@gmail.com>
2024-09-22 23:04:40 -0300

    [feat] put route

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+
 db = SQLAlchemy()
 
 
@@ -7,8 +9,8 @@ class Todo (db.Model):
     TodoID = db.Column(db.Integer, primary_key=True)
     TodoTitle = db.Column(db.String(50), nullable=False)
     TodoDescription = db.Column(db.String(150), nullable=True)
-    TodoStartDate = db.Column(db.DateTime, nullable=False)
-    TodoEndDate = db.Column(db.DateTime, nullable=False)
+    TodoStartDate = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
+    TodoEndDate = db.Column(db.DateTime, nullable=True)
 
 
     def toJson(self):
--- routes/task_routes.py
+++ routes/task_routes.py
@@ -1,14 +1,42 @@
 from app import app
+from datetime import datetime
 from flask import request
-from models import Todo
+from models import db, Todo
 
 
-@app.route('/api/task', methods=['GET'])
+@app.route('/api/task', methods=['GET', 'PUT'])
 def index ():
     taskID = request.args.get('taskID')
-    if not (taskID):
-        tasks = Todo.query.all() 
-        return {"tasks": [task.toJson() for task in tasks]}
-    else:
-        tasks = Todo.query.filter_by(TodoID = taskID)
-        return {"tasks": [task.toJson() for task in tasks]}
+    if request.method == 'GET':
+        if not (taskID):
+            tasks = Todo.query.all() 
+            return {"tasks": [task.toJson() for task in tasks]}
+        else:
+            tasks = Todo.query.filter_by(TodoID = taskID)
+            return {"tasks": [task.toJson() for task in tasks]}
+    
+    elif request.method == 'PUT':
+
+        data = request.get_json()
+        task = Todo.query.filter_by(TodoID=data['ID']).first()
+
+        if not task:
+            return {"error": "Task not found"}, 404
+        
+        # Update the task attributes with new data
+        if 'Title' in data:
+            task.Title = data['Title']
+        if 'Description' in data:
+            task.Description = data['Description']
+        
+        #task.StartDate = datetime.now()
+        
+        #task.EndDate = datetime.now()
+        
+        
+        try:
+            db.session.commit()        
+            return task.toJson(), 200  # Return updated task
+        except Exception as e:
+            db.session.rollback()  # Rollback the session on error
+            return {"error": str(e)}, 500  # Return the error message
